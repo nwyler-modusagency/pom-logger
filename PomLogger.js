bad61047e41701bfd2e908a20bf80f3032c50dc1
Nicolas  Wyler <nwyler@modusagency.com>
2018-05-15 15:05:35 -0300

    check if logger exists path

--- PomLogger.js
+++ PomLogger.js
@@ -9,8 +9,8 @@ class PomLogger {
             const err = 'You must specify the logger you want to use.';
             throw err;
         } else {
-            let loggerPath = path.resolve(`./loggers/${Logger.name}.js`);
-            if (process.env.NODE_ENV === 'production') loggerPath = path.resolve(`./node_modules/pom-logger/loggers/${Logger.name}.js`);
+            let loggerPath = path.resolve(`./node_modules/pom-logger/loggers/${Logger.name}.js`);
+            if (process.env.NODE_ENV === 'test') loggerPath = path.resolve(`./loggers/${Logger.name}.js`);
             if (!fs.existsSync(loggerPath)) {
                 const err = 'The logger specified  was not found.';
                 throw err;
