01a69369da297a5781a6a0126513ed9b931aed6f
Nicolas  Wyler <nwyler@modusagency.com>
2018-05-15 14:30:34 -0300

    get path of loggers depending environment

--- PomLogger.js
+++ PomLogger.js
@@ -9,7 +9,8 @@ class PomLogger {
             const err = 'You must specify the logger you want to use.';
             throw err;
         } else {
-            const loggerPath = path.resolve(`./loggers/${Logger.name}.js`);
+            let loggerPath = path.resolve(`./loggers/${Logger.name}.js`);
+            if (process.env.NODE_ENV === 'production') loggerPath = path.resolve(`./node_modules/pom-logger/loggers/${Logger.name}.js`);
             if (!fs.existsSync(loggerPath)) {
                 const err = 'The logger specified  was not found.';
                 throw err;
